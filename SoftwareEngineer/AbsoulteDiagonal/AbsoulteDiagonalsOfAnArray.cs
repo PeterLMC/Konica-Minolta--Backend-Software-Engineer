76eef01879e5222a2898f389cc4e9f91e1b3ad52
PeterLMC <mc.pedrol@gmail.com>
2021-12-03 17:07:28 +0100

    Add files via upload

--- SoftwareEngineer/AbsoulteDiagonal/AbsoulteDiagonalsOfAnArray.cs
+++ SoftwareEngineer/AbsoulteDiagonal/AbsoulteDiagonalsOfAnArray.cs
@@ -30,9 +30,67 @@ namespace AbsoulteDiagonal
     {
         public static int DiagonalDifference(List<List<int>> arr) 
         {
-            throw new NotImplementedException();
+
+            if (arr == null)
+                throw new ArgumentNullException(String.Format("DiagonalDifference: Null List"));
+
+
+            if (arr.Count == 0)
+                throw new ArgumentException(String.Format("DiagonalDifference: Empty List"));
+
+            int size = arr[0].FirstOrDefault();
+
+
+            var matrix = arr;
+            matrix.RemoveAt(0);
+
+            if (matrix.Count == 0)
+                throw new ArgumentException(String.Format("DiagonalDifference: Bad Array length data"));
+
+            foreach (List<int> vector in matrix)
+            {
+                if (vector.Count != size)
+                    throw new ArgumentException(String.Format("DiagonalDifference: Not a square Array"));
+            }
+
+            int diagonalSum = DiagonalSum(matrix);
+            int antiDiagonalSum = AntiDiagonalSum(matrix);
+
+            int result = diagonalSum - antiDiagonalSum;
+
+            return result < 0 ? (result * -1) : result;
+
         }
 
+
+
+
+        public static int DiagonalSum (List<List<int>> arr)
+        {
+
+            int sum = 0;
+            int position = 0;
+            foreach (List<int> vector in arr)
+            {
+                sum += vector[(position)];
+                position ++; 
+            }
+
+            return sum;
+        }
+
+        public static int AntiDiagonalSum(List<List<int>> arr)
+        {
+            int sum = 0;
+            int position = arr.Count() - 1;
+            foreach (List<int> vector in arr)
+            {
+                sum += vector[position];
+                position--;
+            }
+
+            return sum;
+        }
 
     }
 }
